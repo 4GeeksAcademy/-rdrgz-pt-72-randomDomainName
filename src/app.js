e1dd08a920b229a05a82da302357a02781b3e012
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-08-29 19:38:14 +0000

    needs more work

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+var one = ["The", "Our", "Their", "This"];
+var two = ["great", "big", "wrathful", "meek"];
+var three = ["jogger", "racoon", "passion", "estate"];
+var four = [".com", ".org", ".gov", ".net"];
 
-const pronoun = ['The', 'Our', 'Their', 'This'];
-const adj = ['great', 'big', 'wrathful', 'meek'];
-const noun = ['jogger', 'racoon','passion', 'estate'];
-const ending = ['.com', '.org', '.gov', '.net'];
-for (let i = 0; i < pronoun.length; i++) {
-  console.log(pronoun[i], adj[i],noun[i],ending[i]);
-}
+var all = [one, two, three, four];
+
+for (var i = 0; i < all.length; i++) {
+  var domainName = all[i];
+  return = domainName;
