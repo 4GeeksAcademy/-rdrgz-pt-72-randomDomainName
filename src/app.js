7a0719cfd42da7d13086db26b9a17b9a1570c757
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-08-29 21:43:41 +0000

    end

--- src/app.js
+++ src/app.js
@@ -5,13 +5,11 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-var one = ["The", "Our", "Their", "This"];
-var two = ["great", "big", "wrathful", "meek"];
-var three = ["jogger", "racoon", "passion", "estate"];
-var four = [".com", ".org", ".gov", ".net"];
+const one = ["the", "our", "their", "this"];
+const two = ["great", "big", "wrathful", "meek"];
+const three = ["jogger", "racoon", "passion", "estate"];
+const four = [".com", ".org", ".gov", ".net"];
 
-var all = [one, two, three, four];
-
-for (var i = 0; i < all.length; i++) {
-  var domainName = all[i];
-  return = domainName;
+for (i = 0 < Math.floor(Math.random); any.length; i++) {
+  console.log(one, two, three, four)[i];
+}
