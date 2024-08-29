b6e75327ef0d2b290b14389a9191ef7c14416998
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-08-29 19:02:35 +0000

    end

--- src/app.js
+++ src/app.js
@@ -5,32 +5,11 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-}
-const myArray () ={
-  let pronoun = ['The', 'Our', 'Their', 'This'];
-  let adj = ['great', 'big', 'wrathful', 'meek'];
-  let noun = ['jogger', 'racoon','passion', 'estate'];
-}
 
-function getRandomNumber(_min, _max) {
-  return Math.floor(Math.random() * 6);
+const pronoun = ['The', 'Our', 'Their', 'This'];
+const adj = ['great', 'big', 'wrathful', 'meek'];
+const noun = ['jogger', 'racoon','passion', 'estate'];
+const ending = ['.com', '.org', '.gov', '.net'];
+for (let i = 0; i < pronoun.length; i++) {
+  console.log(pronoun[i], adj[i],noun[i],ending[i]);
 }
-
-function getRandom(anyArray) {
-  let max = anyArray.lenght - 1;
-  let min = 0;
-  let random = getRandomNumber(min, max);
-  return anyArray[random];
-}
-
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-  document.querySelector("#weirdness").innerHTML =
-    getRandom(pronoun) +
-    " " +
-    getRandom(adj) +
-    " " +
-    getRandom(noun)
-};
