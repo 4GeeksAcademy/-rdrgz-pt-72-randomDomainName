d74c0a6b7ab314d1402aeee5b3f5b02ae3def6e2
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-08-31 03:44:13 +0000

    done

--- src/app.js
+++ src/app.js
@@ -5,24 +5,11 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-//let one = ["the", "our", "their", "this"];
-//let two = ["great", "big", "wrathful", "meek"];
-//let three = ["jogger", "racoon", "passion", "estate"];
-//let four = [".com", ".org", ".gov", ".net"];
-
-//const arrays = () => {
-//for (i = 0; i < array.length; ) {
-//Math.floor(Math.random)[i];
-//i++;
-//return one, two, three, four;
-//}
-//};
-//console.log(arrays);
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
+let pronoun = ["the", "our", "their", "this"];
+let adj = ["great", "big", "wrathful", "meek"];
+let noun = ["logger", "human", "passion", "estate"];
 let convertedNoun = noun.map(domainHackNoun);
-// let extension = [".com", ".net", ".us", ".io"];
+
 let extension = noun.map(domainHackExtension);
 
 function domainHackExtension(noun) {
@@ -36,12 +23,9 @@ function domainHackNoun(noun) {
 window.onload = function() {
   //write your code here
   let domainNames = "";
-  // for (let e = 0; e < extension.length; e++) {
   for (let p = 0; p < pronoun.length; p++) {
     for (let a = 0; a < adj.length; a++) {
       for (let n = 0; n < convertedNoun.length; n++) {
-        // for (let n = 0; n < noun.length; n++) {
-        // domainNames +="<li>" + pronoun[p] + adj[a] + noun[n] + extension[e] + "</li>";
         domainNames +=
           "<li>" +
           pronoun[p] +
@@ -51,7 +35,7 @@ window.onload = function() {
           "</li>";
       }
     }
-    // }
+    //}
   }
   console.log(domainNames);
 
