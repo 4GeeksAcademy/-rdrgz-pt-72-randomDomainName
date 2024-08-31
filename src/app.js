1a2882369e8a6af1f3e9856f41704b4e6e211342
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-08-31 00:57:02 +0000

    done?

--- src/app.js
+++ src/app.js
@@ -6,15 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 let one = ["the", "our", "their", "this"];
-
 let two = ["great", "big", "wrathful", "meek"];
-
 let three = ["jogger", "racoon", "passion", "estate"];
-
 let four = [".com", ".org", ".gov", ".net"];
 
-for (i = 0; i < any.length; ) {
-  Math.floor(Math.random);
-  i++;
-}
-console.log(one, two, three, four);
+const arrays = () => {
+  for (i = 0; i < array.length; ) {
+    Math.floor(Math.random)[i];
+    i++;
+    return one, two, three, four;
+  }
+};
+console.log(arrays);
